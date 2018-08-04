e82c2042928ec5bb925f0a7444abf1cc1771ceff
Chris <christoleavenow@gmail.com>
2018-08-04 08:32:26 +0800

    init

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+from lxml import etree
+
+
+def get_ip():
+    url = "http://ip.chinaz.com/"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
+    }
+    proxies = {"https": "1.119.129.2:8080"}
+    response = requests.get(url, headers=headers, proxies=proxies)
+    html = response.text
+    text = etree.HTML(html)
+    result = text.xpath('//*[@id="rightinfo"]/dl//text()')
+    return result
+
+
+if __name__ == "__main__":
+    # print(get_ip())
+    # ip address
+    print(get_ip()[1], ": ", get_ip()[3])
+    # address
+    print(get_ip()[5], ": ", get_ip()[7])
+    # os
+    # print(get_ip()[10], ": ", get_ip()[12])
+    # User-Agent
+    # print(get_ip()[20], ": ", get_ip()[22], get_ip()[23])
